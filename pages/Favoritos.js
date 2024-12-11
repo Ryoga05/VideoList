0a5483cd8ccc51d81d9d807533912f2203ec4b8b
Ryoga05 <ryoga20082005@gmail.com>
2024-12-11 10:34:30 +0100

    Afegir els videos a Favoritos.js

--- pages/Favoritos.js
+++ pages/Favoritos.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import Menu from '../components/Menu';
-import AddButton from '../components/AddButton';
 
 import { FIREBASE_STORAGE } from '../firebaseConfig'; // Importa la configuración de Firebase
 import { collection, getDocs, updateDoc, deleteDoc, doc } from 'firebase/firestore';
+
+import Menu from '../components/Menu';
+import AddButton from '../components/AddButton';
+import Video from '../components/Video';
 
 
 export default function Favoritos({ navigation }) {
@@ -13,6 +15,14 @@ export default function Favoritos({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Favoritos</Text>
+      <ScrollView style={styles.scrollContainer} contentContainerStyle={styles.videoList}>
+        <Video type="YouTube" />
+        <Video type="Instagram" />
+        <Video type="YouTube" />
+        <Video type="YouTube" />
+        <Video type="YouTube" />
+        <Video type="YouTube" />
+      </ScrollView>
       <AddButton/>
       <Menu/>
     </View>
@@ -23,12 +33,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#1C1C1C',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     padding: 16,
   },
   title: {
     color: 'white',
     fontSize: 32,
-  }
+    marginBottom: 20,
+    marginTop: 50,
+  },
+  scrollContainer: {
+    flex: 1,
+    width: '100%',
+    marginBottom: 80,
+  },
+  videoList: {
+  },
 });
