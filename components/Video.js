97786f08f46e89ee285cee4123cee669ec4e405e
Ryoga05 <ryoga20082005@gmail.com>
2024-12-18 10:26:39 +0100

    Añadir gradientes a los componentes Video.js

--- components/Video.js
+++ components/Video.js
@@ -3,13 +3,14 @@ import { View, StyleSheet, TouchableOpacity, Image, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import placeholderImage from '../assets/placeholder.jpg';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const Video = ({ image, type, onPress }) => {
   return (    
-  <View style={styles.container}>
+  <View>
     {type === 'YouTube' ? (
       // Contenido para "YouTube"
-      <>
+      <View style={styles.containerYoutube}>
         <Image
         style={styles.image}
         source={image ? { uri: image } : placeholderImage} 
@@ -21,21 +22,28 @@ const Video = ({ image, type, onPress }) => {
         <TouchableOpacity style={styles.iconContainer} onPress={onPress}>
           <Icon name="play-arrow" size={36} color="white" />
         </TouchableOpacity>
-      </>
+      </View>
     ) : type === 'Instagram' ? (
       // Contenido para "Instagram"
-      <>
-        <TouchableOpacity style={styles.instagram}>
-          <FontAwesome name="instagram" size={64} color="white" />
-        </TouchableOpacity>
-        <View style={styles.textContainer}>
-          <Text style={styles.text}>Title</Text>
-          <Text style={styles.text}>Description</Text>
-        </View>
-        <TouchableOpacity style={styles.iconContainer} onPress={onPress}>
+      <LinearGradient
+        colors={['#5B51D8', '#C13584', '#FD1D1D', '#F77737', '#FFDC80']} // Colores del gradiente
+        start={{ x: 0, y: 0 }}  // Empieza en la parte izquierda
+        end={{ x: 1, y: 0 }}    // Termina en la parte derecha
+        style={styles.gradientBorder}
+      >
+        <View style={styles.containerInstagram}>
+          <TouchableOpacity style={styles.instagram}>
+            <FontAwesome name="instagram" size={64} color="white" />
+          </TouchableOpacity>
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>Title</Text>
+            <Text style={styles.text}>Description</Text>
+          </View>
+          <TouchableOpacity style={styles.iconContainer} onPress={onPress}>
             <Icon name="play-arrow" size={36} color="white" />
-        </TouchableOpacity>
-      </>
+          </TouchableOpacity>
+        </View>
+      </LinearGradient>
     ) : (
       // Contenido predeterminado en caso de que el type no sea ni "YouTube" ni "Instagram"
       <Text style={styles.text}>No content available</Text>
@@ -45,7 +53,7 @@ const Video = ({ image, type, onPress }) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
+  containerYoutube: {
     flex: 1,
     backgroundColor: '#4B4B4B',
     flexDirection: 'row',
@@ -53,6 +61,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingVertical: 30,
     borderRadius: 20,
+    borderColor: 'red',
+    borderWidth: 2,
+    marginBottom: 30,
+  },
+  containerInstagram: {
+    flex: 1,
+    backgroundColor: '#4B4B4B', // Fondo del contenedor interno
+    borderRadius: 17, // Un radio menor que el borde exterior
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingVertical: 30,
+    width: '100%',
+    height: '100%',
+  },
+  gradientBorder: {
+    padding: 2, // Espacio para el borde exterior del gradiente
+    borderRadius: 20, // Bordes redondeados
     marginBottom: 30,
   },
   image: {
